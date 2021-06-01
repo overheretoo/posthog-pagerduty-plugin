41e1de526a3d63f25bf7bb18fab0f60c2d5d8087
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-06-01 10:40:21 +0300

    First version of plugin

--- index.js
+++ index.js
@@ -1,28 +1,107 @@
-// Some internal library function
-async function getRandomNumber() {
-    return 4 // remove this line to get an actual random number from random.org – caution, rate limited to 10 events/s!
-    const response = await fetch(
-        'https://www.random.org/integers/?num=1&min=1&max=1000000000&col=1&base=10&format=plain&rnd=new'
+async function runEveryMinute(meta) {
+    const activeIncidentKey = await meta.cache.get("pagerduty_active_incident")
+    const isInError = await isTrendErroring(meta)
+
+    // console.log({ activeIncidentKey, isInError })
+
+    if (activeIncidentKey && !isInError) {
+        await resolvePagerduty(activeIncidentKey, meta)
+        console.log('Resolved pagerduty incident', activeIncidentKey)
+    } else if (!activeIncidentKey && isInError) {
+        const key = await triggerPagerduty(meta)
+        console.log('Triggered pagerduty incident', key)
+    } else if (isInError) {
+        console.log('Pagerduty incident is active, ignoring error for now')
+    } else {
+        console.log('All good! 😍')
+    }
+}
+
+async function isTrendErroring(meta) {
+    const { data } = await getTrend(meta)
+    return !data.slice(-2).some((value) =>
+        !dataPointInError(value, parseFloat(meta.config.threshold), meta.config.operator)
     )
-    return parseInt(await response.text())
 }
 
-async function setupPlugin({ config }) {
-    console.log(`Setting up the plugin:\n${config.greeting}`)
+function dataPointInError(value, threshold, operator) {
+    if (operator.startsWith('≤')) {
+        return value <= threshold
+    } else {
+        return value >= threshold
+    }
 }
 
-async function processEvent(event, { config, cache }) {
-    const counterValue = (await cache.get('greeting_counter', 0))
-    cache.set('greeting_counter', counterValue + 1)
-    if (!event.properties) event.properties = {}
-    event.properties['greeting'] = config.greeting
-    event.properties['greeting_counter'] = counterValue
-    event.properties['random_number'] = await getRandomNumber()
-    return event
+async function getTrend(meta) {
+    const response = await fetch(meta.config.posthogTrendUrl, {
+        headers: {
+            authorization: `Bearer ${meta.config.posthogApiKey}`
+        }
+    })
+
+    if (!response.ok) {
+        throw Error(`Error from PostHog API: status=${response.status} response=${await response.text()}`)
+    }
+
+    const results = await response.json()
+
+    console.log('Got PostHog trends response', results)
+    return results.result[0]
 }
 
-// The famed Hello World plugin itself
-module.exports = {
-    setupPlugin,
-    processEvent
+async function triggerPagerduty(meta) {
+    const response = await fetch('https://events.pagerduty.com/v2/enqueue', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'accept': 'application/vnd.pagerduty+json;version=2',
+        },
+        body: JSON.stringify({
+            "routing_key": meta.config.pagerdutyIntegrationKey,
+            "event_action": "trigger",
+            "payload": {
+                "summary": `${meta.config.pagerdutyIncidentSummary} - query returned 0`,
+                "source": meta.config.posthogHost,
+                "severity": "critical",
+            },
+            "links": [
+                {
+                    "href": meta.config.posthogTrendUrl,
+                    "text": "Posthog Trends API query url"
+                }
+            ],
+            "custom_details": {
+                "operator": meta.config.operator,
+                "threshold": meta.config.threshold
+            }
+        })
+    })
+
+    if (!response.ok) {
+        throw Error(`Error from PagerDuty API: status=${response.status} response=${await response.text()}`)
+    }
+
+    // console.log('Got PagerDuty response', { status: response.status, text: await response.clone().text() })
+
+    const { dedup_key } = await response.json()
+    await meta.cache.set("pagerduty_active_incident", dedup_key)
+
+    return dedup_key
+}
+
+async function resolvePagerduty(incidentKey, meta) {
+    const response = await fetch('https://events.pagerduty.com/v2/enqueue', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'accept': 'application/vnd.pagerduty+json;version=2',
+        },
+        body: JSON.stringify({
+            "routing_key": meta.config.pagerdutyIntegrationKey,
+            "event_action": "resolve",
+            "dedup_key": incidentKey,
+        })
+    })
+
+    await meta.cache.set("pagerduty_active_incident", null)
 }
